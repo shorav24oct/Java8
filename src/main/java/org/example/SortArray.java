e964c6625f5dd0a27c696145e1ed37e87c0512df
shorav24oct <sharmasaurav142@gmail.com>
2025-12-28 22:17:45 +0530

    Descending order

--- src/main/java/org/example/SortArray.java
+++ src/main/java/org/example/SortArray.java
@@ -1,11 +1,23 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortArray {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
         functionalStyle(arr);
+        descendingOrder(arr);
+    }
+
+    private static void descendingOrder(int[] arr) {
+        int[] sortedDesc = Arrays
+                .stream(arr)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
+        System.out.println(Arrays.toString(sortedDesc));
     }
 
     private static void functionalStyle(int[] arr) {
