c9f800f899d461c294bb0c095378725b6e31c8a7
shorav24oct <sharmasaurav142@gmail.com>
2025-12-28 22:03:36 +0530

    sort using functional style

--- src/main/java/org/example/SortArray.java
+++ src/main/java/org/example/SortArray.java
@@ -1,9 +1,16 @@
 package org.example;
 
-public class SortArray
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        functionalStyle(arr);
+    }
+
+    private static void functionalStyle(int[] arr) {
+        int[] sorted = Arrays.stream(arr).sorted().toArray();
+
+        System.out.println(Arrays.toString(sorted));
     }
 }
