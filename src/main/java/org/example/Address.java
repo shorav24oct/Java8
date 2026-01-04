b7c5b2f2c824ea7cdccfe76ff86a05c3830329bc
shorav24oct <sharmasaurav142@gmail.com>
2026-01-04 20:22:19 +0530

    my own immutable class

---
+++ src/main/java/org/example/Address.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Address {
+    String city;
+    Integer pin;
+
+    public Address(String city, Integer pin) {
+        this.city = city;
+        this.pin = pin;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getPin() {
+        return pin;
+    }
+
+    public void setPin(Integer pin) {
+        this.pin = pin;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", pin=" + pin +
+                '}';
+    }
+}
--- src/main/java/org/example/MyImmutable.java
+++ src/main/java/org/example/MyImmutable.java
@@ -8,11 +8,13 @@ public final class MyImmutable {
     private final String name;
     private final Date doj;
     private final List<Integer> mobile;
+    private final Address address;
 
-    public MyImmutable(String name, Date doj, List<Integer> mobile) {
+    public MyImmutable(String name, Date doj, List<Integer> mobile, Address address) {
         this.name = name;
         this.doj = doj;
         this.mobile = mobile;
+        this.address = address;
     }
 
     public String getName() {
@@ -28,24 +30,35 @@ public final class MyImmutable {
         return new ArrayList<>(mobile);
     }
 
+    public Address getAddress() {
+        // return address;
+        return new Address(address.getCity(), address.getPin());
+    }
+
     @Override
     public String toString() {
         return "MyImmutable{" +
                 "name='" + name + '\'' +
                 ", doj=" + doj +
                 ", mobile=" + mobile +
+                ", address=" + address +
                 '}';
     }
 
     public static void main(String[] args) {
+        Address address1 = new Address("Pune", 144041);
+
         MyImmutable myImmutable = new MyImmutable("shorav", new Date(),
-                Arrays.stream(new Integer[]{123, 456}).collect(Collectors.toList()));
+                Arrays.stream(new Integer[]{123, 456}).collect(Collectors.toList()), address1);
         System.out.println(myImmutable);
 
         myImmutable.getDoj().setDate(15);
         System.out.println(myImmutable);
 
         myImmutable.getMobile().add(789);
+        System.out.println(myImmutable);
+
+        myImmutable.getAddress().setCity("Noida");
         System.out.println(myImmutable);
     }
 }
