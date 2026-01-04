fe2c5b42e1b658297939cb22baa66504bad4163a
shorav24oct <sharmasaurav142@gmail.com>
2026-01-04 21:53:23 +0530

    use Comparable to sort emplo

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Employee implements Comparable<Employee> {
+    private String name;
+    private int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        if (this.id == o.id)
+            return 0;
+        else if (this.id > o.id) {
+            return 1;
+        } else
+            return -1;
+    }
+
+    public static void main(String[] args) {
+        List<Employee> employeeList = new ArrayList<>();
+
+        Employee employee1 = new Employee("shorav", 123);
+        Employee employee2 = new Employee("sharma", 789);
+        Employee employee3 = new Employee("sks", 456);
+
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        employeeList.add(employee3);
+
+        Collections.sort(employeeList);
+
+        System.out.println(employeeList);
+    }
+}
