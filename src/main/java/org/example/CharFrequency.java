8572196bb795e00e2d7b1a7ca5d15f491e6732a4
shorav24oct <sharmasaurav142@gmail.com>
2026-02-04 05:24:37 +0530

    Output in Same Order as String (LinkedHashMap)

--- src/main/java/org/example/CharFrequency.java
+++ src/main/java/org/example/CharFrequency.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.LinkedHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,20 @@ public class CharFrequency {
     public static void main(String[] args) {
         String str = "walmart";
 
+        /*
+        Java streams work on primitive int values for characters to support Unicode,
+        so we convert using (char) c.
+         */
         str.chars()
                 .mapToObj(c -> (char)c)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .forEach((k, v) -> System.out.println(k + " -> " + v));
+
+        System.out.println("*************");
+
+        str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .forEach((k,v) -> System.out.println(k + " -> " + v));
     }
 }
