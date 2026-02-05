8c3126d5617b8da73737bb8f8ef1fbfbfccaa0c4
shorav24oct <sharmasaurav142@gmail.com>
2026-02-05 06:54:37 +0530

    Highest paid employee in each department

--- src/main/java/org/example/classobj/HighestPaidEmployee.java
+++ src/main/java/org/example/classobj/HighestPaidEmployee.java
@@ -1,9 +1,20 @@
 package org.example.classobj;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class HighestPaidEmployee {
     public static void main(String[] args) {
         List<Employee> employees = EmpDB.allEmp();
+
+        Map<String, Optional<Employee>> highestPaid = employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDept, Collectors.maxBy(
+                        Comparator.comparingDouble(Employee::getSalary)
+                )));
+
+        highestPaid.forEach((d,e) -> System.out.println(d + " -> " + e.get().getName()));
     }
 }
