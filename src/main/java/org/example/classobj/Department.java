7c2e6896f96e48ff1a625f31328c08143a556ca6
shorav24oct <sharmasaurav142@gmail.com>
2026-02-05 06:35:05 +0530

    add new Emp and Dept classes

---
+++ src/main/java/org/example/classobj/Department.java
@@ -0,0 +1,8 @@
+package org.example.classobj;
+
+import java.util.List;
+
+public class Department {
+    private String name;
+    private List<Employee> employees;
+}
---
+++ src/main/java/org/example/classobj/Employee.java
@@ -0,0 +1,86 @@
+package org.example.classobj;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private double salary;
+    private String dept;
+
+    public Employee(int id, String name, int age, double salary, String dept) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.dept = dept;
+    }
+
+    public Employee() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && age == employee.age && Double.compare(salary, employee.salary) == 0 && Objects.equals(name, employee.name) && Objects.equals(dept, employee.dept);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, salary, dept);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", dept='" + dept + '\'' +
+                '}';
+    }
+}
