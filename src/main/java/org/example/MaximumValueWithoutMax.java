cfd7889cf9f8fd777aee9beadb39382359d8d1c7
shorav24oct <sharmasaurav142@gmail.com>
2026-02-05 06:16:35 +0530

    Maximum value without Collections.max

---
+++ src/main/java/org/example/MaximumValueWithoutMax.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Maximum value without Collections.max
+ */
+public class MaximumValueWithoutMax {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(4,7,2,9,1);
+
+        Integer ans = list.stream().reduce(Integer::max).get();
+
+        System.out.println(ans);
+    }
+}
